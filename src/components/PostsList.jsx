26707d38c0222f3b3258606f8f85b7cda6b93a86
csumaoang <csumaoang@academic.rrc.ca>
2024-09-05 13:36:46 -0500

    Added react demo components

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import classes from "./PostsList.module.css";
+import Post from "./Post.jsx";
+import Modal from "./Modal";
+import NewPost from "./NewPost.jsx";
+
+function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  let modalContent;
+
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+      {modalContent}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
